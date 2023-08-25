46e383d6bef39f3a8ce5f1eb7e1bcd2e8027f3ff
Rory Williams <rorywilliams183@gmail.com>
2023-08-25 15:55:28 +0100

    minor change

--- get_data.py
+++ get_data.py
@@ -3,7 +3,7 @@ import os
 
 
 WORDS = np.loadtxt('words.txt', dtype = str)
-PATH = 'C:/Users/roryw/Documents/Wordle Solver'
+PATH = '#'
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
