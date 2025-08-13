4f95f2d70e1ae2c89bcfee987bcb599daa6a57a5
Rory Williams <rorywilliams183@gmail.com>
2025-08-13 17:20:33 +0100

    fixed some logic with double letters and words in wordlist are only allowed to be guessed

--- wordle_game.py
+++ wordle_game.py
@@ -7,6 +7,7 @@ class WordleGame:
         self.answer = answer
         self.word_list = np.loadtxt('words.txt', dtype = str)
         self.guesses = np.array([])
+        self.guessed = ""
         self.output = []
         self.sim = sim
         self.solved = False
@@ -28,16 +29,27 @@ class WordleGame:
         if self.answer != None and guessed != None:
             # Checks for greens
             for i in range(len(self.answer)):
+                # Gets indexes of double letters in the answer
+                mult_letters = [j for j, l in enumerate(self.answer) if l == guessed[i]]
+                mult_guess_l =  [j for j, l in enumerate(guessed) if l == guessed[i]]
+
                 if self.answer[i] == guessed[i]:
                     self.output[i] = '!'
             
                 elif self.output[i] != '!':
                     # Checks for yellows
                     if self.answer.__contains__(guessed[i]):
-                        self.output[i] = '%'
+                        if len(mult_letters) >= len(mult_guess_l):
+                            self.output[i] = '%'
+                        else:
+                            self.output[i] = '_'
+                            
+                        # self.output[i] = '%'
                     else:
                         # If letter not in word then grey
                         self.output[i] = '_'
+                        
+        return self.output
 
     def check(self, guessed):
         if guessed == self.answer:
@@ -46,7 +58,7 @@ class WordleGame:
 
 
 def checkInput(guessed):
-    if len(guessed) != 5:
+    if len(guessed) != 5 and (guessed not in wordle.word_list):
         return False
     if not guessed.isalpha():
         return False
@@ -59,7 +71,7 @@ if __name__ == "__main__":
     if not wordle.sim:
         if wordle.answer == None:
             wordle.answer = wordle.pickRandomWord()
-
+        
         print("Wordle\n")
         print(" ! = Green Letter ")
         print(" % = Yellow Letter ")
@@ -69,14 +81,14 @@ if __name__ == "__main__":
 
         while not wordle.solved and wordle.num_guesses < 6:
             print(f"\nGuesses left: {6 - wordle.num_guesses}")
-            guessed = input("Enter a word: ").lower()
+            wordle.guessed = input("Enter a word: ").lower()
 
-            while not checkInput(guessed):
-                guessed = input("Enter a valid word: ")
+            while not checkInput(wordle.guessed):
+                wordle.guessed = input("Enter a valid word: ")
 
-            wordle.guess(guessed)
-            print(" ".join(wordle.output))
-            wordle.solved = wordle.check(guessed)
+            wordle.guess(wordle.guessed)
+            print("".join(wordle.output))
+            wordle.solved = wordle.check(wordle.guessed)
             wordle.num_guesses += 1
 
         if wordle.solved:
