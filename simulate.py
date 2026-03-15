e1d35a9cc8af512e737dab633efbb9829519c117
Rory Williams <rorywilliams183@gmail.com>
2026-03-15 17:16:21 +0000

    created function to create a matrix to lookup results of all wordle guesses

--- simulate.py
+++ simulate.py
@@ -5,7 +5,7 @@ from wordle_solver import *
 
 
 WORDS = np.loadtxt('words.txt', dtype = str)
-PATH = 'C:/Users/roryw/Documents/Wordle Solver'
+PATH = 'C:/Users/roryw/Documents/Wordle Solver/data'
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
--- solver/train.py
+++ solver/train.py
@@ -3,30 +3,50 @@ import numpy as np
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from filters import WordleFilter
+from wordle_game import WordleGame
 
+WORD_LIST = sorted(set(np.loadtxt("data/wordlists/answer_wordlist.txt", dtype=str))) # All words available in wordle
+WORD_LIST_LEN = len(WORD_LIST)
+
+PATH = 'C:/Users/roryw/Documents/Wordle Solver/data'
 
 class WordleTrain:
 
     def __init__(self):
         pass
 
-    # Finds bits gained for current guess
-    # I = -log2(1/p(x))
-    # p(x) = words remaining / total words (wordlist length)
-    def information(self):
-        pass
-
     # Finds expected value (information entropy) for guess
     # i.e Average information gained for a particular guess
     # E(I) = Sumof (p(x)) * log2(1/p(x))
-    def entropy(self):
-        pass
+    def entropy(self, pattern_counts):
+        H = 0
+        total = sum(pattern_counts.values())
+        for c in pattern_counts.values():
+            p = c / total
+            if p > 0:
+                H -= p * np.log2(p)
 
-    # Might not be needed
-    # finds all combinations for a guess
-    def combinations(self):
-        pass
+        return H
+    
+    def lookup_table(self):
+        '''
+        Creates matrix where matrix[guess][answer] is the comparison of words using 
+        wordle game logic
+        Matrix is saved and saves time since the comparisons between guesses and answers never change
+        '''
+        
+        m = [['']*WORD_LIST_LEN for _ in range(WORD_LIST_LEN)]
+        wordle = WordleGame()
 
-    # Gets entropy values for every word as a first guess
-    def find_first_guess(self):
-        pass
+        for g, guess in enumerate(list(WORD_LIST)):
+            for a, answer in enumerate(list(WORD_LIST)):
+                m[g][a] = "".join(wordle.pattern(guess, answer))
+
+        np.save(f"{PATH}/guess_matrix.npy", m)
+
+
+            
+if __name__ == "__main__":
+    train = WordleTrain()
+    guess_matrix = np.load("data/guess_matrix.npy", allow_pickle=True)
+    print(guess_matrix)
