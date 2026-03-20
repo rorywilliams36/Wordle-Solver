5249faa8cdbb89e8e8b40b9f452a0a24fe900a85
Rory Williams <rorywilliams183@gmail.com>
2026-03-20 17:58:18 +0000

    input vaildation for argparser

--- main.py
+++ main.py
@@ -16,6 +16,21 @@ def get_wordlist(word_list_arg):
         return ANSWER_LIST
     return WORD_LIST
 
+def validate_word_inputs(word_list, input_word):
+    if input_word is not None:
+        if (input_word in word_list) or ((len(input_word) == 5) and (input_word.isalpha())):
+            return input_word
+        print(f'{input_word}: is not valid to use. Your input will be randomised from the word list')
+    return None
+
+def validate_answer(word_list, input_word):
+    if input_word is not None:
+        if input_word in word_list:
+            return input_word
+        print(f'{input_word}: is not valid to use. Your input will be randomised from the word list')
+    return None
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description = "Wordle Game and Solver")
 
@@ -26,21 +41,24 @@ if __name__ == "__main__":
     parser.add_argument('-gs', '--game_solver', action='store_true', help="Run Game and Solver")
 
     # Set Variables
-    parser.add_argument('--set_answer', type=str, help="Set Wordle Answer", default=None)
+    parser.add_argument('--set_answer', type=str, help="Set Wordle Answer (Only used when p the game)", default=None)
     parser.add_argument('--set_wordlist', type=str, choices=['answers', 'guesses', 'most_common'], help="Set wordlist", default='most_common')
     parser.add_argument('--set_first_guess', type=str, help='Sets first guess used in solver', default=None)
     
     # Get Args
     args = parser.parse_args()
-    answer = args.set_answer
+
     word_list = get_wordlist(args.set_wordlist)
-    
+    answer = validate_answer(word_list, args.set_answer)
+    first_guess = validate_word_inputs(word_list, args.set_first_guess)
+    print(first_guess)
+
     if args.game:
-        run_game(word_list, args.set_answer)
+        run_game(word_list, answer)
 
     if args.gather_data:
         run_gather_data(word_list)
 
     if args.solver:
-        run_solver(word_list, args.set_first_guess)
+        run_solver(word_list, first_guess)
 
