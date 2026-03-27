2f6a172aff1cec89c8fe7bb8434a32fb49d2cc98
Rory Williams <rorywilliams183@gmail.com>
2026-03-27 16:15:36 +0000

    rename file and moved relevant functions to it

---
+++ solver/data_utils.py
@@ -0,0 +1,140 @@
+import os
+import sys
+import json
+import numpy as np
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+from wordle_game import WordleGame
+
+PATH = f'{os.path.abspath(os.getcwd())}/data'
+
+
+def create_guess_matrix(word_list):
+    '''
+    Creates matrix where matrix[guess][answer] is the comparison of words using 
+    wordle game logic
+    Matrix is saved and saves time since the comparisons between guesses and answers never change
+    '''
+
+    # Initalise matrix
+    word_list_len = len(word_list)
+    guess_matrix = np.empty((word_list_len, word_list_len), dtype=np.dtype('U100'))
+    wordle = WordleGame()
+
+    word_list = list(word_list)
+
+    # Fill matrix
+    for g, guess in enumerate(word_list):
+        for a, answer in enumerate(word_list):
+            res = "".join(wordle.pattern(guess, answer))
+            guess_matrix[g][a] = res
+
+    guess_matrix_path = f'{PATH}/guess_matrix.npy'
+    try:
+        with open(guess_matrix_path, 'wb') as f:
+            np.save(f, guess_matrix)
+            f.close()
+    except Exception as e:
+        print(f"Error: {e}")
+
+def find_first_guess(word_list, word_to_index, guess_matrix):
+    '''
+    Calculates the expected information gained for every word as a first guess
+    '''
+    from solver.train import entropy
+
+    guess_entropy = {}
+
+    for guess in word_list:
+        pattern_counts = {}
+        for answer in word_list:
+            # Lookup result for guess and answer
+            g_idx = word_to_index[guess]
+            a_idx = word_to_index[answer]
+            res = guess_matrix[g_idx][a_idx]
+
+            # Increment result occurence
+            if pattern_counts.get(res):
+                pattern_counts[res] += 1
+            else:
+                pattern_counts[res] = 1
+
+        # Calculate entropy for word
+        H = entropy(pattern_counts)
+        guess_entropy[guess] = H
+
+    entropy_file_path = f"{PATH}/first_guess_entropy.json"
+
+    # Save values as JSON
+    try:
+        with open(entropy_file_path, 'w') as f:
+            json.dump(guess_entropy, f)
+    except Exception as e:
+        print(f'Error: {e}')
+
+def apply_sigmoid():
+    ''' Applies sigmoid function to wordlist to calculate a probability for word being the answer '''
+
+    # open wordlist
+    try:
+        with open(f"{PATH}/wordlists/words.txt", "r") as f:
+            most_likely = f.readlines()
+            f.close()
+    except FileNotFoundError:
+        print('File not found')
+    except Exception as e:
+        print(f'Error: {e}')
+
+
+    # Apply sigmoid function (z)
+    most_likely.reverse()
+    most_likely_len = len(most_likely)
+    x = np.linspace(-5, 10, most_likely_len)
+    z = 1/(1 + np.exp(-x))
+
+    # Attach sigmoid value to words
+    word_prob = {}
+    for w in range(most_likely_len):
+        word = most_likely[w].strip()
+        word_prob[word] = z[w]
+
+    # Save values
+    word_prob_path = f"{PATH}/word_probabilites.json"
+    try:
+        with open(word_prob_path, 'w') as f:
+            json.dump(word_prob, f)
+    except Exception as e:
+        print(f'Error: {e}')
+
+# Load Data
+def load_guess_matrix(word_list):
+    '''
+    Loads the guess matrix containing all results for each guess and answer
+    Creates an indexing tool so that the guess matrix can be correctly accessed
+    '''
+    # Load guess lookup matrix
+    guess_matrix = []
+    word_to_index = {}
+    try:
+        guess_matrix = np.load(f"{PATH}/guess_matrix.npy", allow_pickle=True)
+        word_to_index = {w:i for i,w in enumerate(word_list)} # Indexing table for each word
+    except FileNotFoundError:
+        print('File not found')
+    except Exception as e:
+        print(f'Error: {e}')
+    return guess_matrix, word_to_index
+
+def load_json(f_name):
+    '''
+    Loads the JSON file given by f_name
+    '''
+    json_file = {}
+    try:
+        with open(f"{PATH}/{f_name}.json", "r") as f:
+            json_file = json.load(f)
+    except FileNotFoundError:
+        print('File not found')
+    except Exception as e:
+        print(f'Error: {e}')
+    return json_file
+
--- solver/train.py
+++ solver/train.py
@@ -5,16 +5,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from solver.filters import WordleFilter
-import solver.load_data as _load
+import solver.data_utils as d_utils
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-from wordle_game import WordleGame
-
-WORD_LIST = sorted(set(np.loadtxt("data/wordlists/answer_wordlist.txt", dtype=str)))
-WORD_LIST_LEN = len(WORD_LIST)
 MAX_GUESSES = 6
 
 PATH = f'{os.path.abspath(os.getcwd())}/data'
+GUESS_ENTROPYS = 'first_guess_entropy'
+WORD_PROBS = 'word_probabilites'
+
 
 class WordleSolver:
     def __init__(self, guess_matrix, word_to_index, first_guess_entropy, word_probs, word_list: set = WORD_LIST):
@@ -24,90 +22,7 @@ class WordleSolver:
         self.word_probs = word_probs
         self.word_list = word_list
 
-    def entropy(self, pattern_counts):
-        '''
-        Finds expected value (information entropy) for guess
-        i.e Average information gained for a particular guess
-        H = - (Sumof [(p(x)) * log2(p(x))])
-
-        Args:
-            pattern_counts: dict containing counters for occurences of each result/pattern
-
-        Returns:
-            Entropy value (int)
-        '''
-        H = 0
-        counts = pattern_counts.values()
-        total = sum(counts)
-        for count in counts:
-            p = count / total
-            if p > 0:
-                H -= p * np.log2(p)
-
-        return H
     
-    def create_guess_matrix(self):
-        '''
-        Creates matrix where matrix[guess][answer] is the comparison of words using 
-        wordle game logic
-        Matrix is saved and saves time since the comparisons between guesses and answers never change
-        '''
-
-        # Initalise matrix
-        word_list_len = len(self.word_list)
-        guess_matrix = np.empty((word_list_len, word_list_len), dtype=np.dtype('U100'))
-        wordle = WordleGame()
-
-        word_list = list(self.word_list)
-
-        # Fill matrix
-        for g, guess in enumerate(word_list):
-            for a, answer in enumerate(word_list):
-                res = "".join(wordle.pattern(guess, answer))
-                guess_matrix[g][a] = res
-
-        guess_matrix_path = f'{PATH}/guess_matrix.npy'
-        try:
-            with open(guess_matrix_path, 'wb') as f:
-                np.save(f, guess_matrix)
-                f.close()
-        except Exception as e:
-            print(f"Error: {e}")
-
-    def find_first_guess(self):
-        '''
-        Calculates the expected information gained for every word as a first guess
-        '''
-
-        guess_entropy = {}
-
-        for guess in self.word_list:
-            pattern_counts = {}
-            for answer in self.word_list:
-                # Lookup result for guess and answer
-                g_idx = self.word_to_index[guess]
-                a_idx = self.word_to_index[answer]
-                res = self.guess_matrix[g_idx][a_idx]
-
-                # Increment result occurence
-                if pattern_counts.get(res):
-                    pattern_counts[res] += 1
-                else:
-                    pattern_counts[res] = 1
-
-            # Calculate entropy for word
-            H = self.entropy(pattern_counts)
-            guess_entropy[guess] = H
-
-        entropy_file_path = f"{PATH}/first_guess_entropy.json"
-
-        # Save values as JSON
-        try:
-            with open(entropy_file_path, 'w') as f:
-                json.dump(guess_entropy, f)
-        except Exception as e:
-            print(f'Error: {e}')
-
     def solve(self, first_guess: str = None):
         '''
         Plays every possible Wordle game using a set word as the first guess
@@ -126,8 +41,8 @@ class WordleSolver:
 
         num_guesses = []
         guess_record = {}
-        guess_distribution = [0,0,0,0,0,0]
-        for answer in list(self.word_list)[:10]:
+        guess_distribution = [0,0,0,0,0,0,0]
+        for answer in list(self.word_list):
             filters = WordleFilter(self.word_list)
 
             guess = first_guess
@@ -135,8 +50,8 @@ class WordleSolver:
             pos_guesses_remain = len(self.word_list)
             guess_num = 0
             solved = False
-            print('================')
-            print(answer)
+            # print('================')
+            # print(answer)
 
             # Set containing all submitted guesses
             # avoids same guess being submitted if contains the same letters
@@ -152,7 +67,7 @@ class WordleSolver:
                 g_idx = self.word_to_index[guess]
                 a_idx = self.word_to_index[answer]
                 res = self.guess_matrix[g_idx][a_idx]
-                print(guess_num, guess, res, entropy, pos_guesses_remain)
+                # print(guess_num, guess, res, entropy, pos_guesses_remain)
                 # guess_record[answer].append((guess_num, guess))
 
                 # Set guess as submitted 
@@ -180,7 +95,12 @@ class WordleSolver:
             num_guesses.append(guess_num)
             if guess_num <=6:
                 guess_distribution[guess_num-1] += 1
-        print(num_guesses)
+            else:
+                solved = True
+                guess_distribution[-1] += 1
+                print(answer)
+
+        # print(num_guesses)
         print(guess_distribution)
         print(np.mean(num_guesses))
 
@@ -211,56 +131,46 @@ class WordleSolver:
                     pattern_counts[res] = 1
 
             # Calculate entropy for word
-            H = self.entropy(pattern_counts)
+            H = entropy(pattern_counts)
             pos_guess_entropy[guess] = H
 
         return pos_guess_entropy
 
 
-def apply_sigmoid():
-    ''' Applies sigmoid function to wordlist to calculate a probability for word being the answer '''
+def entropy(pattern_counts):
+    '''
+    Finds expected value (information entropy) for guess
+    i.e Average information gained for a particular guess
+    H = - (Sumof [(p(x)) * log2(p(x))])
 
-    # open wordlist
-    try:
-        with open(f"{PATH}/wordlists/words.txt", "r") as f:
-            most_likely = f.readlines()
-            f.close()
-    except FileNotFoundError:
-        print('File not found')
-    except Exception as e:
-        print(f'Error: {e}')
+    Args:
+        pattern_counts: dict containing counters for occurences of each result/pattern
 
+    Returns:
+        Entropy value (int)
+    '''
+    H = 0
+    counts = pattern_counts.values()
+    total = sum(counts)
+    for count in counts:
+        p = count / total
+        if p > 0:
+            H -= p * np.log2(p)
 
-    # Apply sigmoid function (z)
-    most_likely.reverse()
-    most_likely_len = len(most_likely)
-    x = np.linspace(-5, 10, most_likely_len)
-    z = 1/(1 + np.exp(-x))
-
-    # Attach sigmoid value to words
-    word_prob = {}
-    for w in range(most_likely_len):
-        word = most_likely[w].strip()
-        word_prob[word] = z[w]
-
-    # Save values
-    word_prob_path = f"{PATH}/word_probabilites.json"
-    try:
-        with open(word_prob_path, 'w') as f:
-            json.dump(word_prob, f)
-    except Exception as e:
-        print(f'Error: {e}')
+    return H
 
 # Main Functions
 def run_gather_data(word_list):
     solver = WordleSolver(None, {}, {}, word_list)
-    solver.create_guess_matrix()
-    solver.first_guess_entropy
+    d_utils.create_guess_matrix(word_list)
+    guess_matrix, word_to_index = d_utils.load_guess_matrix(word_list)
+    d_utils.find_first_guess(word_list, word_to_index, guess_matrix)
+    d_utils.apply_sigmoid()
 
 def run_solver(word_list, first_guess):
-    guess_matrix, word_to_index = _load.load_guess_matrix(word_list)
-    first_guess_entropy = _load.load_first_guess_entropy()
-    word_probs = _load.load_word_probs()
+    guess_matrix, word_to_index = d_utils.load_guess_matrix(word_list)
+    first_guess_entropy = d_utils.load_json(GUESS_ENTROPYS)
+    word_probs = d_utils.load_json(WORD_PROBS)
 
     if (len(guess_matrix) == 0) or (len(first_guess_entropy) == 0) or (len(word_probs) == 0):
         print('Error loading data')
